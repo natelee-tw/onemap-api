69eaf1e52e5f29062b400eea7335dc76a70df593
Mengyong Lee <34482689+mylee16@users.noreply.github.com>
2020-03-08 10:12:36 +0800

    Update HDB data extract.py

--- HDB data extract.py
+++ HDB data extract.py
@@ -3,6 +3,7 @@ import requests
 import openpyxl
 import time
 
+#Open worksheet 
 wb = openpyxl.load_workbook('List of HDB Address.xlsx')
 sheet = wb['sheet1']
 
@@ -15,11 +16,16 @@ for row in range(2,sheet.max_row+1):
         elif count < 200: #retrive 
             query_address=sheet['A' + str(row)].value
 
+#import request 
+#formulate query string       
             query_string='https://developers.onemap.sg/commonapi/search?searchVal='+str(query_address)+'&returnGeom=Y&getAddrDetails=Y&pageNum=1'
             resp = requests.get(query_string)
         
+#Convert JSON into Python Object 
             data=json.loads(resp.content)
+        #print(type(data))
 
+#Extract data from JSON
             try:
                 sheet['B' + str(row)]=data['results'][0]['LONGITUDE']
                 sheet['C' + str(row)]=data['results'][0]['LATITUDE']
